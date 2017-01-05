60e52e631ccd53560513a28c96b914a6b46645a5
Brain Pritt and suzi rubino <suzi@>
2017-01-05 15:36:46 -0800

    finish styling game

--- js/dinoLorem-interface.js
+++ js/dinoLorem-interface.js
@@ -1,6 +1,7 @@
 var Dino = require('./../js/dinosaur.js').dinoModule;
 //global variables
 var guessArray = [];
+var idArray = [];
 var finalDino = [];
 var myDino = new Dino(1, 6);
 var totalMatches = 0;
@@ -17,18 +18,26 @@ var displayDinos = function(dinoList){
   //gameplay starts here
   $("#game-board").children().click(function(){
     var dino = $(this).attr("value");
+    $(this).addClass("white");
     guessArray.push(dino);
+    idArray.push(this.id);
     if (guessArray.length === 2) {
       var matchingDinos = myDino.isMatch(guessArray);
-      if (matchingDinos === true){
-        $("." + guessArray[0]).removeClass();
+      if(idArray[0] === idArray[1]){
+        guessArray = [];
+        idArray = [];
+      }else if (matchingDinos === true){
+        $("." + guessArray[0]).removeAttr("value").addClass("whiteBkgrd");
         totalMatches += 1;
         if (totalMatches === 6){
-          $("#game-board").append("<h3>You Did It!</h3>");
+          $(".gameOver").append("<h3>You Did It!</h3>");
         }
         guessArray = [];
+        idArray = [];
       } else{
         guessArray = [];
+        idArray = [];
+        $("p").removeClass("white");
       }
     }
   });
