522f37d3bce5b23936be5d9285a8ac5e55485396
Brian Pritt and Suzi Rubino <suzi@>
2017-01-04 16:47:25 -0800

    add deck build and shuffle method

--- js/dinoLorem-interface.js
+++ js/dinoLorem-interface.js
@@ -1,11 +1,17 @@
 var Dino = require('./../js/dinosaur.js').dinoModule;
+
+
+
 var finalDino = [];
+
 var displayDinos = function(dinoList){
   finalDino = dinoList[0];
   var dinoId = 0;
-  finalDino.forEach(function(dinoName) {
-    $("#game-board").append('<div id="' + dinoId + '" class = "' + dinoName + '">' + dinoName + '</div>');
+
+  dinoList.forEach(function(dinoName) {
+    $("#game-board").append('<div id="' + dinoId + '" class = "name ' + dinoName + '">' + dinoName + '</div>');
     dinoId +=1;
+
   });
 };
 
@@ -13,5 +19,7 @@ $(document).ready(function() {
   var words = 4;
   var myDino = new Dino(1, words);
   var response = myDino.getDinos(displayDinos);
-
+  $("#0, #1, #2, #3, #4, #5, #6, #7").click(function(){
+    console.log("hi");
+  });
 });
--- js/dinosaur.js
+++ js/dinosaur.js
@@ -4,11 +4,31 @@ function Dino(paragraphSize, nameList) {
 }
 Dino.prototype.getDinos = function(displayFunction){
    $.get('http://dinoipsum.herokuapp.com/api/?format=json&words=' + this.nameList + '&paragraphs=' + this.paragraphSize).then(function(response){
-     displayFunction(response);
-    console.log(response);
+    var finalDino = response[0];
+    //double deck
+    var newDeck = [];
+    for(i = 0; i <2; i++){
+      finalDino.forEach(function(dinoName){
+        newDeck.push(dinoName);
+      });
+    }
+    //fisher-yates shuffle https://bost.ocks.org/mike/shuffle/
+    var m = newDeck.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+    // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+        // And swap it with the current element.
+        t = newDeck[m];
+        newDeck[m] = newDeck[i];
+        newDeck[i] = t;
+      }
+    displayFunction(newDeck);
   }).fail(function(error){
     console.log(error);
   });
-}
+};
+
 
 exports.dinoModule = Dino;
